5730f5dd1081b4e5548b11de5d444e64d5873fdc
Tanmoy-Santra <tanmoysantra911@gmail.com>
2024-05-27 09:26:16 +0530

    done

--- ass6/ass6_client.c
+++ ass6/ass6_client.c
@@ -2,37 +2,39 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<sys/ssockt.h>
+#include<sys/socket.h>
 #include<sys/types.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
-#define SERVER_PORT 5000
-#define SERVER_IP "192.168.5.36"
-#define CLIENT_PORT 5001
+#define SERVER_PORT 4000
+#define SERVER_IP "127.0.0.1"
+#define CLIENT_PORT 4001
 #define CLIENT_IP "127.0.0.1"
 int main(){
     int sd;
-    char str[512];
-    struct aockaddr_in client,server;
-
+    char str[512],str1[512];
+    struct sockaddr_in client,server;
     bzero((char*)&client,sizeof(client));
     client.sin_family=AF_INET;
     client.sin_port=htons(CLIENT_PORT);
     client.sin_addr.s_addr=inet_addr(CLIENT_IP);
-
     bzero((char*)&server,sizeof(server));
     server.sin_family=AF_INET;
     server.sin_port=htons(SERVER_PORT);
     server.sin_addr.s_addr=inet_addr(SERVER_IP);
-
     sd=socket(AF_INET,SOCK_STREAM,0);
+    bind(sd,(struct sockaddr*)&client,sizeof(client));
     connect(sd,(struct sockaddr*)&server,sizeof(server));
 
     do{
-        printf("\nenter the msg :");
-        scanf("%s",&str);
-        send(sd,str,strlen(str)+1,0);
-    }while(strcmp(str,"stop"))
+        printf("\nenter the msg : ");
+        scanf("%s",str1);
+        send(sd,str1,strlen(str1)+1,0);
+        memset(str,0x0,512);
+        recv(sd,str,512,0);
+        printf("\nreceived msg :%s",str);
+
+    }while(strcmp(str1,"stop"));
     close(sd);
 }
 
--- ass6/ass6_server.c
+++ ass6/ass6_server.c
@@ -1,45 +1,39 @@
-/server6
-
+//server6
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<sys/ssockt.h>
+#include<sys/socket.h>
 #include<sys/types.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<time.h>
-#define SERVER_PORT 5000
-#define SERVER_IP "192.168.5.36"
-
+#define SERVER_PORT 4000
+#define SERVER_IP "127.0.0.1"
 int main(){
-    struct aockaddr_in client,server;
+    struct sockaddr_in client,server;
     int sd,nsd,clen=sizeof(client);
-    char str[512],str1[512]="invalid request";   
-    int *ptr;
+    char str[512],str1[512]="invalid request";
+    char *ptr;
     time_t ti;
     bzero((char*)&server,sizeof(server));
     server.sin_family=AF_INET;
     server.sin_port=htons(SERVER_PORT);
     server.sin_addr.s_addr=inet_addr(SERVER_IP);
-
     sd=socket(AF_INET,SOCK_STREAM,0);
     bind(sd,(struct sockaddr*)&server,sizeof(server));
-    listen(sd,5)
+    listen(sd,5);
     while(1){
          nsd = accept(sd, (struct sockaddr*)&client, &clen);
     do{
         memset(str,0x0,512);
         recv(nsd,str,512,0);
-        if(strcmp(st,"time")==0){
+        if(strcmp(str,"time")==0){
             ti=time(NULL);
             ptr=ctime(&ti);
-            send(nsd,str,strlen(str)+1,0);
+            send(nsd,ptr,strlen(ptr)+1,0);
         }
         else{
-             send(nsd,str1,strlen(str1)+1,0);
-        }
+             send(nsd,str1,strlen(str1)+1,0);        }
 
-    }while(strcmp(str,"stop"))
-    close(nsd);
-}
-}
+    }while(strcmp(str,"stop"));
+    close(nsd);}}
